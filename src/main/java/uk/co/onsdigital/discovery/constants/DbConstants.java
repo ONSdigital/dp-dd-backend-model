21d29a58d1ac44fb8348cacfb1ac5f12c0db9ee8
Matt Smith <matthew.smith@thinkinglogic.com>
2017-02-07 10:07:59 +0000

    added flyway config and sql files

---
+++ src/main/java/uk/co/onsdigital/discovery/constants/DbConstants.java
@@ -0,0 +1,11 @@
+package uk.co.onsdigital.discovery.constants;
+
+/**
+ * Constants related to the db, and flyway.
+ */
+public class DbConstants {
+
+    /** The location of the sql scripts used to create/migrate the database using flyway. */
+    public static final String SQL_SCRIPTS_LOCATION = "classpath:/db/migration";
+
+}
---
+++ src/main/resources/db/migration/V01_005__hierarchy_CL_0000737_Prodcom_Elements.sql
@@ -0,0 +1,20 @@
+insert into hierarchy (id, name, type) values ('CL_0000737', 'Prodcom Elements', 'classification');
+
+
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('944430b4-f90f-4e9b-b1b9-b4ddec69e9b2', 'CL_0000737', 'CI_0008131', null, 'Volume', null, 3);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('1c803ccf-4e82-4726-b9dc-7ba046df7dc0', 'CL_0000737', 'CI_0021409', null, 'Unit Value', null, 4);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('200e9837-5ddb-4696-8223-37e9ed460414', 'CL_0000737', 'CI_0021509', null, 'UK manufacturer sales of individual products listed in this product group', null, 6);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('5aa688eb-dcce-4aaa-8fd6-e6244728cb0b', 'CL_0000737', 'CI_0021510', null, 'All other income', null, 7);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('b827dc63-c4da-4575-8921-9e4618b3278b', 'CL_0000737', 'CI_0021511', null, 'Total UK manufacturer sales of products in this product group', null, 5);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('28b94fbd-1ba9-4ab2-be20-9b9710019d9a', 'CL_0000737', 'CI_0021512', null, 'Merchanted goods', null, 8);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('bc9ccccf-adcb-4683-a584-0ae015bb273e', 'CL_0000737', 'CI_0021513', null, 'Work done', null, 9);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('b78bbf7c-8fc8-4bd3-9f8d-2d479f4c2983', 'CL_0000737', 'CI_0021514', null, 'Waste Products', null, 10);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('c61781cb-84e5-4ca0-934f-4e0bf5397443', 'CL_0000737', 'CI_0021515', null, 'Non production income', null, 11);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('3e72e6b8-2c7c-451a-b8c2-4c4416cf1307', 'CL_0000737', 'CI_0021516', null, 'Net carry in/carry out', null, 12);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('0e086cf4-e1f7-4769-8142-f9448bcd2afe', 'CL_0000737', 'CI_0021517', null, 'Total turnover of businesses classified to this industry', null, 13);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('19dfd937-af13-47da-8f02-7a12adfaa93b', 'CL_0000737', 'CI_0033579', null, 'Value (£000)', null, 1);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('838e9527-00dd-49dc-aa5f-0e067a33beda', 'CL_0000737', 'CI_0042393', null, 'Value', null, 20);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('b1d5ac8a-e02b-41a4-88e3-908de4600e58', 'CL_0000737', 'CI_0042394', null, 'Standard Error', null, 21);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('dc4692db-0221-46f5-98c9-2bcd8d390c39', 'CL_0000737', 'CI_0042395', null, 'CV', null, 22);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('7c144ec8-47f9-4916-914d-cfde7a313e9c', 'CL_0000737', 'CI_0042396', null, 'NUMEG', null, 23);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('646fd5c1-1546-43be-b0d7-291a08e28cd7', 'CL_0000737', 'CI_0042398', null, 'EMP', null, 24);
