eff9da162ae7df47ea91c62568d1277f2cde5a08
Neil Madden <Neil.E.Madden@gmail.com>
2017-02-21 16:35:00 +0000

    Add a default value to avoid blowing up for existing data.

--- src/main/resources/db/migration/V001_014__multiple_hierarchies.sql
+++ src/main/resources/db/migration/V001_014__multiple_hierarchies.sql
@@ -2,7 +2,7 @@
 ALTER TABLE dimension DROP CONSTRAINT fk_dimension_hierarchy_id;
 ALTER TABLE dimension DROP COLUMN hierarchy_id;
 -- Add a generic dimension type column ("geography", "standard", etc).
-ALTER TABLE dimension ADD COLUMN type VARCHAR(255) NOT NULL;
+ALTER TABLE dimension ADD COLUMN type VARCHAR(255) NOT NULL DEFAULT 'standard';
 
 -- Add missing FK from dimension_value to dimension.
 ALTER TABLE dimension_value ADD CONSTRAINT FK_dimension_value_dimension FOREIGN KEY (dimensional_data_set_id, name) REFERENCES dimension(dimensional_data_set_id, name);
