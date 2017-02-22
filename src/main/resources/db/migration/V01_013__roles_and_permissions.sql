0bb2e77baefb7ee57e627d9219b1f5ff2034b35c
Mohammed Abdulrazeg <github@mzawi.me>
2017-02-22 08:23:47 +0000

    Added job creation tables' ownership to job_creator

--- src/main/resources/db/migration/V01_013__roles_and_permissions.sql
+++ src/main/resources/db/migration/V01_013__roles_and_permissions.sql
@@ -1,6 +1,9 @@
 -- Job Creator API user. Needs CRUD permissions on its own job status tables, minimal SELECT permissions otherwise.
 GRANT ALL PRIVILEGES ON TABLE job, file_status, job_file_status TO job_creator;
 GRANT SELECT ON TABLE dimensional_data_set TO job_creator; -- Needed to lookup S3 URL
+ALTER TABLE job owner TO job_creator;
+ALTER TABLE job_file_status owner TO job_creator;
+ALTER TABLE file_status owner TO job_creator;
 
 -- Metadata API user. Needs read-only permissions on most of the schema.
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO dd_api;
