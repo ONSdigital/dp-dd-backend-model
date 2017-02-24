4ad9ab6a6a24e7e24ec825eb4d1386ead5cd54d5
Neil Madden <Neil.E.Madden@gmail.com>
2017-02-24 09:34:05 +0000

    Add submittedAt field for files and query to delete expired files.

--- src/main/java/uk/co/onsdigital/discovery/model/FileStatus.java
+++ src/main/java/uk/co/onsdigital/discovery/model/FileStatus.java
@@ -5,12 +5,19 @@ import lombok.Data;
 import lombok.NonNull;
 import lombok.experimental.Tolerate;
 
+import java.util.Date;
+
 
 /**
  * Indicates the status of a particular output file in a job.
  */
 @Data @Entity @Table(name = "file_status")
+@NamedQueries({
+        @NamedQuery(name = FileStatus.DELETE_EXPIRED_FILES, query = "DELETE FROM FileStatus fs WHERE fs.submittedAt < :before")
+})
 public class FileStatus {
+    public static final String DELETE_EXPIRED_FILES = "FileStatus.deleteExpired";
+
     @Id
     private @NonNull String name;
 
@@ -18,6 +25,9 @@ public class FileStatus {
     private @NonNull Status status = Status.PENDING;
 
     private String url;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date submittedAt;
 
     @Tolerate
     public FileStatus() {
