e9fa56d75b44cdf28bf3f91eb04a176d5915a6ac
Neil Madden <Neil.E.Madden@gmail.com>
2017-02-24 09:59:07 +0000

    Default to current timestamp for existing files.

--- src/main/resources/db/migration/V01_015_add_submitted_at_timestamp.sql
+++ src/main/resources/db/migration/V01_015_add_submitted_at_timestamp.sql
@@ -1,2 +1,2 @@
-ALTER TABLE file_status ADD COLUMN submitted_at TIMESTAMP(6) NULL;
+ALTER TABLE file_status ADD COLUMN submitted_at TIMESTAMP(6) NOT NULL DEFAULT current_timestamp(6);
 COMMENT ON COLUMN file_status.submitted_at IS 'Timestamp at which the file was submitted to be generated.';
