6c3469742774b1df1b2314a2d151d76b6d61d087
Matt Smith <matthew.smith@thinkinglogic.com>
2017-03-03 14:19:55 +0000

    #435: Added international returns hierarchies

---
+++ src/main/resources/db/migration/V01_033__CL_0001576_Prices.sql
@@ -0,0 +1,5 @@
+insert into hierarchy (id, name, type) values ('CL_0001576', 'Prices', 'classification');
+
+
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('5ee8e972-65f2-45f7-8aeb-f8d7250839f6', 'CL_0001576', 'CI_0042552', null, 'V - Current prices', null, 0);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('10f6091b-1ae1-4ee4-a2ee-9f1c82e297b8', 'CL_0001576', 'CI_0042638', null, '_Z - Not applicable', null, 1);
