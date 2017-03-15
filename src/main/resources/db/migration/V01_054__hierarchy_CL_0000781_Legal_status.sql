c713167ecda136e3e913db83b83cde12293bf6b6
Matt Smith <matthew.smith@thinkinglogic.com>
2017-03-15 15:04:10 +0000

    #976: Add hierarchies for new datasets

---
+++ src/main/resources/db/migration/V01_054__hierarchy_CL_0000781_Legal_status.sql
@@ -0,0 +1,18 @@
+insert into hierarchy (id, name, type) values ('CL_0000781', 'Legal status', 'classification');
+
+
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('fa630a4f-91bc-447c-9fad-7a5bb6e6918d', 'CL_0000781', 'CI_0005220', null, 'Total', null, 8);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('6b631101-85ba-43f3-904c-938d317b127b', 'CL_0000781', 'CI_0007200', null, '1 - Company (incl. Building Society)', null, 9);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('f8c5d5d3-6df0-40b5-8210-46a53012d14f', 'CL_0000781', 'CI_0007201', null, '2 - Sole Proprietor', null, 10);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('30610d9d-2738-43be-95a0-171827bde972', 'CL_0000781', 'CI_0007202', null, '3 - Partnership', null, 11);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('2e950b2c-ff5a-41fb-8354-f0b75a2e9355', 'CL_0000781', 'CI_0007203', null, '4 - Public Corporation/Nationalised Body', null, 12);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('148e78c2-1833-4852-8f60-ee688b390d07', 'CL_0000781', 'CI_0007204', null, '5 - Central Government', null, 13);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('f7452615-f59d-49e8-a72f-be6dd79162f3', 'CL_0000781', 'CI_0007205', null, '6 - Local Authority', null, 14);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('576ce96c-0d73-4436-a65a-a540e37d03a6', 'CL_0000781', 'CI_0007206', null, '7 - Non-profit Body or Mutual Association', null, 15);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('fd87b1ad-d056-48b8-9aca-5f5ff3d65d12', 'CL_0000781', 'CI_0012141', null, 'Company (incl. Building Society)', null, 1);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('e285217c-f25b-4af0-929a-7d5710b34860', 'CL_0000781', 'CI_0012142', null, 'Sole Proprietor', null, 2);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('a6454896-e7d6-4029-86d9-2ff1ddbec43a', 'CL_0000781', 'CI_0012143', null, 'Partnership', null, 3);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('64a987fe-85a0-48f7-9301-797f823aa9e9', 'CL_0000781', 'CI_0012144', null, 'Public Corporation/Nationalised Body', null, 4);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('70e232e7-f1e8-4bd2-a624-b157b3922bff', 'CL_0000781', 'CI_0012145', null, 'Central Government', null, 5);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('aa0f8bea-84d4-4859-a6a3-e9491f1f2237', 'CL_0000781', 'CI_0012146', null, 'Local Authority', null, 6);
+insert into hierarchy_entry (id, hierarchy_id, code, parent, name, hierarchy_level_type_id, display_order) values ('5ddf78fe-6bf2-4459-86ba-16c72da5fb70', 'CL_0000781', 'CI_0012147', null, 'Non-profit Body or Mutual Association', null, 7);
